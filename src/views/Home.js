be111d12d77c417a13c121921efc80857b30aa4d
Alex Orlet <AlexOrlet89@gmail.com>
2023-01-17 11:58:01 -0800

    more appropriate fake content

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,131 +6,127 @@ export default function Home() {
     <div className="Home share">
       <h2 id="initial-header">Initial header</h2>
       <p>
-        When I first downloaded my singing monsters I would play before and
-        after work and progress just fine, great game to kill time. I showed
-        this game to my colleagues and family, and soon the people around me
-        started enjoying the game with me. During dinner time with the family,
-        we would all play my singing monsters and progress together. This became
-        a fun bonding experience with me and my family, as I am rarely home due
-        to work. This game gave me the much needed time to bond with my wife and
-        kids. The monsters were fun, the breeding was anticipating, the
-        progression was slow yet rewarding. All nice qualities, I found myself
-        especially interested with the monsters, but more specifically the
-        monster called 'toe jammer'. The sound, the appearance of the monster
-        was all very appealing to me. When people asked me what my favourite
-        monster is I would without hesitation respond with 'toe jammer'. He was
-        by far my favourite, I liked the blue translucent colour of it, I liked
-        the funny idea of the character. My wife bought me a toe jammer stuffed
-        toy for my birthday this year, and I couldn't of been more grateful.
-        Now, please spare your criticism on this next part. I'm scared to speak
-        out about this and it's very vulnerable for me. My wife hadn't been home
-        for over a week, she was out on a very extensive business trip. And I
-        was without a doubt very sexually frustrated, it had been a while since
-        me and my wife had shared an intimate moment with each other. And,
-        shamefully I got off to my toe jammer stuffed toy she got for me. I
-        don't know why I did it, why I chose toe jammer to be the subject of my
-        sexual frustration but I did. On nights where my wife wasn't home it
-        became habit to use my toe jammer stuffed toy as an outlet for my needs.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nibh venenatis cras
+        sed felis eget velit aliquet. Etiam non quam lacus suspendisse. Quam
+        viverra orci sagittis eu volutpat odio facilisis mauris. Ut lectus arcu
+        bibendum at varius vel pharetra vel turpis. Volutpat commodo sed egestas
+        egestas fringilla phasellus. Aliquet risus feugiat in ante metus dictum
+        at. Accumsan sit amet nulla facilisi morbi tempus. Nec dui nunc mattis
+        enim ut tellus elementum. Sed elementum tempus egestas sed sed risus
+        pretium quam vulputate. Diam volutpat commodo sed egestas. At auctor
+        urna nunc id. Fames ac turpis egestas sed tempus urna. Fusce id velit ut
+        tortor pretium viverra suspendisse potenti nullam. At tellus at urna
+        condimentum mattis pellentesque. Dignissim suspendisse in est ante in
+        nibh mauris cursus mattis. Laoreet sit amet cursus sit amet dictum sit.
+        Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Quis enim
+        lobortis scelerisque fermentum. Nunc eget lorem dolor sed viverra. Etiam
+        non quam lacus suspendisse faucibus interdum posuere lorem. Id velit ut
+        tortor pretium viverra suspendisse potenti. Amet volutpat consequat
+        mauris nunc congue nisi vitae suscipit. Eu ultrices vitae auctor eu.
+        Diam sit amet nisl suscipit adipiscing bibendum est. Commodo odio aenean
+        sed adipiscing diam donec adipiscing tristique risus. Quis varius quam
+        quisque id diam vel quam elementum. Tincidunt eget nullam non nisi est
+        sit amet facilisis magna. Nascetur ridiculus mus mauris vitae ultricies
+        leo integer. Diam maecenas ultricies mi eget mauris pharetra et ultrices
+        neque. Purus semper eget duis at tellus at. Augue lacus viverra vitae
+        congue eu consequat ac. Vel elit scelerisque mauris pellentesque
+        pulvinar pellentesque. Ac tortor dignissim convallis aenean et tortor at
+        risus. Ut pharetra sit amet aliquam id diam maecenas ultricies mi.
+        Senectus et netus et malesuada fames ac turpis. Massa tincidunt dui ut
+        ornare. Lacus sed turpis tincidunt id aliquet risus feugiat. Et tortor
+        consequat id porta nibh venenatis cras sed felis. Eget sit amet tellus
+        cras adipiscing enim eu turpis. At quis risus sed vulputate odio ut
+        enim. Ipsum dolor sit amet consectetur adipiscing elit pellentesque
+        habitant morbi. Bibendum ut tristique et egestas quis ipsum suspendisse.
+        Lorem mollis aliquam ut porttitor leo a diam. A iaculis at erat
+        pellentesque. Pretium quam vulputate dignissim suspendisse in est ante.
+        Nisl vel pretium lectus quam id leo. Ut placerat orci nulla pellentesque
+        dignissim enim. Amet luctus venenatis lectus magna fringilla urna.
+        Semper risus in hendrerit gravida rutrum quisque non. Facilisis volutpat
+        est velit egestas dui. Nulla malesuada pellentesque elit eget gravida
+        cum sociis natoque. Vestibulum rhoncus est pellentesque elit ullamcorper
+        dignissim cras tincidunt. Sed velit dignissim sodales ut. Habitasse
+        platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper
+        dignissim. Pharetra vel turpis nunc eget. Nunc sed velit dignissim
+        sodales ut eu. Tellus at urna condimentum mattis pellentesque id nibh.
+        Diam ut venenatis tellus in metus vulputate. Pellentesque diam volutpat
+        commodo sed egestas egestas. Nec tincidunt praesent semper feugiat nibh
+        sed. Sed egestas egestas fringilla phasellus faucibus scelerisque
+        eleifend donec. Urna et pharetra pharetra massa massa ultricies mi quis
+        hendrerit. Feugiat scelerisque varius morbi enim nunc. In massa tempor
+        nec feugiat nisl pretium fusce id velit. Aliquet eget sit amet tellus
+        cras. Augue interdum velit euismod in pellentesque massa placerat duis
+        ultricies. Ante in nibh mauris cursus mattis molestie. Nulla pharetra
+        diam sit amet nisl suscipit. Morbi tincidunt augue interdum velit
+        euismod in pellentesque. Risus in hendrerit gravida rutrum quisque non
+        tellus orci. Sapien et ligula ullamcorper malesuada proin. Elit
+        ullamcorper dignissim cras tincidunt lobortis feugiat. Neque viverra
+        justo nec ultrices dui sapien eget mi. Etiam erat velit scelerisque in
+        dictum non consectetur a. Enim ut sem viverra aliquet eget sit amet
+        tellus. Leo integer malesuada nunc vel risus. Justo donec enim diam
+        vulputate ut pharetra sit amet aliquam. Arcu non odio euismod lacinia at
+        quis risus. Pulvinar proin gravida hendrerit lectus.{" "}
       </p>
       <h2 id="second-header">double header</h2>
       <p>
-        I spoke to my wife about wanting more intimate times with her, in which
-        she obliged, she shared that she felt the same way. It was refreshing to
-        know my feelings were reciprocated, on that night me and my wife spent
-        the night together engaging in intercourse. However at times, I would
-        imagine that my wife was actually toe jammer. I felt guilty for this
-        obviously, it was a weird sexual fantasy that I just couldn't get rid
-        of. More and more nights we shared I just couldn't stop imaging my wife
-        as toe jammer. Something about that character captivated me so sexually,
-        it's so shameful. It really is, and it's greatly embarrassing. One night
-        when me and my wife were having sex I accidentally called her toe
-        jammer, in which her response was shocked and confused. Followed by a
-        very intense lunch conversation on the next day, I opened up about my
-        sexual fantasies revolving toe jammer to my wife and she reassured me
-        that she's okay with it and understands. However, over the following
-        weeks she began to act strange towards me. avoiding me whenever
-        possible, avoiding eye contact, and denying opportunities for intimacy.
-        I brought this up to her one night and she expressed to me that she's
-        struggling to find me attractive after I opened up about my fantasies.
-        It's been a month since that conversation, we're falling out and our
-        kids are distressed. Toe jammer has ruined my love life and I don't know
-        what he'll ruin next. I don't know what to do and I've been greatly
-        stressed out.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sed velit dignissim
+        sodales ut eu sem integer vitae justo. Est sit amet facilisis magna. Ut
+        tellus elementum sagittis vitae et leo duis ut. Massa sapien faucibus et
+        molestie ac feugiat sed lectus. Amet justo donec enim diam vulputate.
+        Adipiscing elit duis tristique sollicitudin nibh sit. Amet consectetur
+        adipiscing elit pellentesque habitant morbi tristique senectus et. Purus
+        non enim praesent elementum facilisis leo vel fringilla. Nulla facilisi
+        morbi tempus iaculis urna id volutpat lacus. Suspendisse potenti nullam
+        ac tortor vitae purus. In ante metus dictum at tempor commodo
+        ullamcorper. Euismod lacinia at quis risus sed vulputate odio. Eu augue
+        ut lectus arcu bibendum at varius vel pharetra.
       </p>
       <h2 id="third-header">Third Header</h2>
       <p>
-        I spoke to my wife about wanting more intimate times with her, in which
-        she obliged, she shared that she felt the same way. It was refreshing to
-        know my feelings were reciprocated, on that night me and my wife spent
-        the night together engaging in intercourse. However at times, I would
-        imagine that my wife was actually toe jammer. I felt guilty for this
-        obviously, it was a weird sexual fantasy that I just couldn't get rid
-        of. More and more nights we shared I just couldn't stop imaging my wife
-        as toe jammer. Something about that character captivated me so sexually,
-        it's so shameful. It really is, and it's greatly embarrassing. One night
-        when me and my wife were having sex I accidentally called her toe
-        jammer, in which her response was shocked and confused. Followed by a
-        very intense lunch conversation on the next day, I opened up about my
-        sexual fantasies revolving toe jammer to my wife and she reassured me
-        that she's okay with it and understands. However, over the following
-        weeks she began to act strange towards me. avoiding me whenever
-        possible, avoiding eye contact, and denying opportunities for intimacy.
-        I brought this up to her one night and she expressed to me that she's
-        struggling to find me attractive after I opened up about my fantasies.
-        It's been a month since that conversation, we're falling out and our
-        kids are distressed. Toe jammer has ruined my love life and I don't know
-        what he'll ruin next. I don't know what to do and I've been greatly
-        stressed out.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sed velit dignissim
+        sodales ut eu sem integer vitae justo. Est sit amet facilisis magna. Ut
+        tellus elementum sagittis vitae et leo duis ut. Massa sapien faucibus et
+        molestie ac feugiat sed lectus. Amet justo donec enim diam vulputate.
+        Adipiscing elit duis tristique sollicitudin nibh sit. Amet consectetur
+        adipiscing elit pellentesque habitant morbi tristique senectus et. Purus
+        non enim praesent elementum facilisis leo vel fringilla. Nulla facilisi
+        morbi tempus iaculis urna id volutpat lacus. Suspendisse potenti nullam
+        ac tortor vitae purus. In ante metus dictum at tempor commodo
+        ullamcorper. Euismod lacinia at quis risus sed vulputate odio. Eu augue
+        ut lectus arcu bibendum at varius vel pharetra.
       </p>
       <h2 id="third-header">Third Header</h2>
       <p>
-        I spoke to my wife about wanting more intimate times with her, in which
-        she obliged, she shared that she felt the same way. It was refreshing to
-        know my feelings were reciprocated, on that night me and my wife spent
-        the night together engaging in intercourse. However at times, I would
-        imagine that my wife was actually toe jammer. I felt guilty for this
-        obviously, it was a weird sexual fantasy that I just couldn't get rid
-        of. More and more nights we shared I just couldn't stop imaging my wife
-        as toe jammer. Something about that character captivated me so sexually,
-        it's so shameful. It really is, and it's greatly embarrassing. One night
-        when me and my wife were having sex I accidentally called her toe
-        jammer, in which her response was shocked and confused. Followed by a
-        very intense lunch conversation on the next day, I opened up about my
-        sexual fantasies revolving toe jammer to my wife and she reassured me
-        that she's okay with it and understands. However, over the following
-        weeks she began to act strange towards me. avoiding me whenever
-        possible, avoiding eye contact, and denying opportunities for intimacy.
-        I brought this up to her one night and she expressed to me that she's
-        struggling to find me attractive after I opened up about my fantasies.
-        It's been a month since that conversation, we're falling out and our
-        kids are distressed. Toe jammer has ruined my love life and I don't know
-        what he'll ruin next. I don't know what to do and I've been greatly
-        stressed out.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sed velit dignissim
+        sodales ut eu sem integer vitae justo. Est sit amet facilisis magna. Ut
+        tellus elementum sagittis vitae et leo duis ut. Massa sapien faucibus et
+        molestie ac feugiat sed lectus. Amet justo donec enim diam vulputate.
+        Adipiscing elit duis tristique sollicitudin nibh sit. Amet consectetur
+        adipiscing elit pellentesque habitant morbi tristique senectus et. Purus
+        non enim praesent elementum facilisis leo vel fringilla. Nulla facilisi
+        morbi tempus iaculis urna id volutpat lacus. Suspendisse potenti nullam
+        ac tortor vitae purus. In ante metus dictum at tempor commodo
+        ullamcorper. Euismod lacinia at quis risus sed vulputate odio. Eu augue
+        ut lectus arcu bibendum at varius vel pharetra.
       </p>
       <h2 id="fourth-header">fourth Header</h2>
       <p>
-        I spoke to my wife about wanting more intimate times with her, in which
-        she obliged, she shared that she felt the same way. It was refreshing to
-        know my feelings were reciprocated, on that night me and my wife spent
-        the night together engaging in intercourse. However at times, I would
-        imagine that my wife was actually toe jammer. I felt guilty for this
-        obviously, it was a weird sexual fantasy that I just couldn't get rid
-        of. More and more nights we shared I just couldn't stop imaging my wife
-        as toe jammer. Something about that character captivated me so sexually,
-        it's so shameful. It really is, and it's greatly embarrassing. One night
-        when me and my wife were having sex I accidentally called her toe
-        jammer, in which her response was shocked and confused. Followed by a
-        very intense lunch conversation on the next day, I opened up about my
-        sexual fantasies revolving toe jammer to my wife and she reassured me
-        that she's okay with it and understands. However, over the following
-        weeks she began to act strange towards me. avoiding me whenever
-        possible, avoiding eye contact, and denying opportunities for intimacy.
-        I brought this up to her one night and she expressed to me that she's
-        struggling to find me attractive after I opened up about my fantasies.
-        It's been a month since that conversation, we're falling out and our
-        kids are distressed. Toe jammer has ruined my love life and I don't know
-        what he'll ruin next. I don't know what to do and I've been greatly
-        stressed out.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sed velit dignissim
+        sodales ut eu sem integer vitae justo. Est sit amet facilisis magna. Ut
+        tellus elementum sagittis vitae et leo duis ut. Massa sapien faucibus et
+        molestie ac feugiat sed lectus. Amet justo donec enim diam vulputate.
+        Adipiscing elit duis tristique sollicitudin nibh sit. Amet consectetur
+        adipiscing elit pellentesque habitant morbi tristique senectus et. Purus
+        non enim praesent elementum facilisis leo vel fringilla. Nulla facilisi
+        morbi tempus iaculis urna id volutpat lacus. Suspendisse potenti nullam
+        ac tortor vitae purus. In ante metus dictum at tempor commodo
+        ullamcorper. Euismod lacinia at quis risus sed vulputate odio. Eu augue
+        ut lectus arcu bibendum at varius vel pharetra.
       </p>
     </div>
   );
