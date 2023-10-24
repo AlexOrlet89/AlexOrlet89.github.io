c5c45c81eb1224a4426d36e8e0b37995ebbadaeb
Alex Orlet <AlexOrlet89@gmail.com>
2023-10-23 18:40:35 -0700

    imported skateboardingHorse from Images and placed in img tag in header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import skateboardingHorse from "./Images/skateboarding_horse_by_micole55_df97eiv-pre.jpg";
 import TableOfContents from "./Components/TableOfContents/TableOfContents";
 import Home from "./views/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="Header">HEAVEN SKATE Header</h1>
+      <h1 id="logoImage" className="Header">
+        <a href="/">
+          <img src={skateboardingHorse} alt="Skateboarding Horse" />
+        </a>
+      </h1>
       {/* <TableOfContents /> */}
       <Home />
     </div>
